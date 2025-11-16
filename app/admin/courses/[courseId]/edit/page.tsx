54613cd5f093e5b166d08f8f50521d8b76722072
Udith Kumar <maddiudith2903@gmail.com>
2025-11-16 18:17:11 +0530

    updated

--- app/admin/courses/[courseId]/edit/page.tsx
+++ app/admin/courses/[courseId]/edit/page.tsx
@@ -13,13 +13,26 @@ export default async function EditRoute({ params }: { params: Params }) {
   return (
     <div>
       <h1 className="text-3xl font-bold mb-8">
-        Edit Course:<span className="text-primary underline">{data.title}</span>
+        Edit Course: {" "}
+        <span className="text-primary underline">{data.title}</span>
       </h1>
       <Tabs defaultValue=" basic-info" className="w-full">
         <TabsList className="grid grid-cols-2 w-full">
           <TabsTrigger value="basic-info">Basic Info</TabsTrigger>
           <TabsTrigger value="Course-Structure">Course Structure</TabsTrigger>
         </TabsList>
+        <TabsContent value="basic-info">
+          <Card>
+            <CardHeader>
+              <CardTitle>Basic info</CardTitle>
+              <CardDescription>Provide basic information about the course</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <EditCourseForm data={data} />
+            </CardContent>
+
+          </Card>
+        </TabsContent>
         <TabsContent value="Course-Structure">
           <Card>
             <CardHeader>
